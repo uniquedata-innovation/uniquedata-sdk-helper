9f1598daafedb4ce0f486ca1909cd90e23e7769c
Jaderson Berti <jaderson.berti@asserttecnologia.com.br>
2025-02-14 21:12:22 -0300

    fix:ajsutes deploy

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,11 @@
 
 echo "maven deploy uniquedata-sdk-helper started";
 
-mvn clean install -Pgithub-sdk-helper-profile
+mvn clean
 
-mvn clean deploy -Pgithub-sdk-helper-profile -X
+mvn install
+
+mvn deploy -Pgithub-sdk-helper-profile
 
 echo "Finish success deploy!";
 
